9ca5ac745e7440ff646f531223c59348cbfbaf7f
vierkantemeter <mark@vierkante-meter.nl>
2014-05-13 15:26:29 +0200

    Update default.inc.php
    Completed and improved the Dutch Quip language file.
    Other files comming soon.

--- core/components/quip/lexicon/nl/default.inc.php
+++ core/components/quip/lexicon/nl/default.inc.php
@@ -22,147 +22,141 @@
  * @package quip
  */
 /**
- * Quip English language file
+ * Quip Dutch language file
  *
  * @package quip
  * @subpackage lexicon
  */
 $_lang['quip'] = 'Quip';
-$_lang['quip_desc'] = 'Een simpel reactie component.';
+$_lang['quip_desc'] = 'Discussies en reacties beheren';
 $_lang['quip.ago'] = ' geleden';
 $_lang['quip.ago_days'] = '[[+days]] dagen';
 $_lang['quip.ago_hours'] = '[[+hours]] uren';
 $_lang['quip.ago_minutes'] = '[[+minutes]] minuten';
 $_lang['quip.ago_seconds'] = '[[+seconds]] seconden';
-$_lang['quip.approve_selected'] = 'Keur selectie goed';
+$_lang['quip.approve_selected'] = 'Selectie goedkeuren';
 $_lang['quip.approved'] = 'Goedgekeurd';
-$_lang['quip.allowed_tags'] = 'Toegestaande tags: [[+tags]]';
+$_lang['quip.allowed_tags'] = 'Toegestane labels: [[+tags]]';
 $_lang['quip.anonymous'] = 'Anoniem';
 $_lang['quip.author'] = 'Auteur';
-$_lang['quip.back_to_threads'] = 'Terug naar de reactie-lijst overzicht';
+$_lang['quip.back_to_threads'] = 'Terug naar de lijst met discussies';
 $_lang['quip.body'] = 'Body';
-$_lang['quip.bulk_actions'] = 'Groep acties';
+$_lang['quip.bulk_actions'] = 'Bulk acties';
 $_lang['quip.close'] = 'Sluit';
 $_lang['quip.comment'] = 'Reactie';
 $_lang['quip.comment_add_new'] = 'Voeg een nieuwe reactie toe:';
-$_lang['quip.comment_approve'] = 'Ractie goedkeuren';
+$_lang['quip.comment_approve'] = 'Reactie goedkeuren';
 $_lang['quip.comment_approve_selected'] = 'Keur geselecteerde reacties goed';
 $_lang['quip.comment_approved'] = 'Reactie goedgekeurd';
 $_lang['quip.comment_approved_msg'] = 'De reactie van [[+name]] op [[+createdon]] is succesvol goedgekeurd.';
-$_lang['quip.comment_delete'] = 'Verwijder reactie';
+$_lang['quip.comment_delete'] = 'Reactie verwijderen';
 $_lang['quip.comment_delete_selected'] = 'Verwijder geseleceerde reacties';
 $_lang['quip.comment_deleted'] = 'Reactie verwijderd';
 $_lang['quip.comment_deleted_msg'] = 'De reactie van [[+name]] op [[+createdon]] is succesvol verwijderd.';
 $_lang['quip.comment_delete_confirm'] = 'Weet u zeker dat u deze reactie wilt verwijderen?';
 $_lang['quip.comment_err_nf'] = 'Reactie niet gevonden.';
 $_lang['quip.comment_err_ns'] = 'Reactie niet gespecificeerd.';
-$_lang['quip.comment_err_remove'] = 'An error occurred while trying to remove the comment.';
-$_lang['quip.comment_err_save'] = 'An error occurred while trying to save the comment.';
-$_lang['quip.comment_remove'] = 'Permanently Remove Comment';
-$_lang['quip.comment_remove_confirm'] = 'Are you sure you want to permanently remove this comment? This cannot be undone.';
-$_lang['quip.comment_remove_selected'] = 'Remove Selected Comments';
-$_lang['quip.comment_unapprove'] = 'Wijs reactie af';
-$_lang['quip.comment_unapprove_selected'] = 'Wijs geselecteerde reacties af';
-$_lang['quip.comment_undelete'] = 'Herstel reactie';
-$_lang['quip.comment_undelete_selected'] = 'Herstel geselecteerde reacties';
-$_lang['quip.comment_update'] = 'Update reactie';
+$_lang['quip.comment_err_remove'] = 'Er ging iets fout bij het verwijderen van de reactie.';
+$_lang['quip.comment_err_save'] = 'Er ging iets fout bij het opslaan van de reactie.';
+$_lang['quip.comment_remove'] = 'Reactie permanent verwijderen';
+$_lang['quip.comment_remove_confirm'] = 'Weet u zeker dat u de reactie permanent wilt verwijderen? Dit kan niet meer ongedaan gemaakt worden.';
+$_lang['quip.comment_remove_selected'] = 'Verwijder geselecteerde reacties.';
+$_lang['quip.comment_unapprove'] = 'Reactie afkeuren';
+$_lang['quip.comment_unapprove_selected'] = 'Keur geselecteerde reacties af';
+$_lang['quip.comment_undelete'] = 'Verwijdering van reactie ongedaan maken';
+$_lang['quip.comment_undelete_selected'] = 'Verwijdering ongedaan maken voor geselecteerde reacties';
+$_lang['quip.comment_update'] = 'Reactie aanpassen';
 $_lang['quip.comment_view'] = 'Bekijk reactie';
-$_lang['quip.comment_will_be_moderated'] = 'Your comment has been submitted, and is now awaiting moderation. You will receive an email when your comment has been approved.';
-$_lang['quip.comments'] = 'Racties';
+$_lang['quip.comment_will_be_moderated'] = 'Uw reactie is ingestuurd en zal z.s.m. worden bekeken. U ontvangt een e-mail als uw reactie is goedgekeurd.';
+$_lang['quip.comments'] = 'Reacties';
 $_lang['quip.delete'] = 'Verwijder';
-$_lang['quip.delete_selected'] = 'Verwijder selectie';
-$_lang['quip.email'] = 'Email';
-$_lang['quip.email_err_ns'] = 'Specificeer een email adres.';
-$_lang['quip.err_fraud_attempt'] = 'Error: IDs of users do not match. Fraud attempt detected.';
-$_lang['quip.err_not_logged_in'] = 'You are not logged in and therefore are not authorized to post comments.';
-$_lang['quip.hide_deleted'] = 'Hide Deleted';
-$_lang['quip.intro_msg'] = 'Manage your Quip comments and threads here.';
-$_lang['quip.ip'] = 'IP';
-$_lang['quip.latest_comments'] = 'Latest Comments';
-$_lang['quip.latest_comments_for'] = 'Latest Comments for';
-$_lang['quip.latest_comments_msg'] = 'View the latest posted comments here.';
-$_lang['quip.login_to_comment'] = 'Please login to comment.';
-$_lang['quip.message_err_ns'] = 'Please specify a message to post.';
-$_lang['quip.moderated'] = 'Moderated';
-$_lang['quip.moderated_desc'] = 'New posts to this thread will be moderated.';
+$_lang['quip.delete_selected'] = 'Selectie verwijderen';
+$_lang['quip.email'] = 'E-mailadres';
+$_lang['quip.email_err_ns'] = 'Specificeer een e-mailadres.';
+$_lang['quip.err_fraud_attempt'] = 'Het ID van de gebruikers komen niet overeen. Dit kan mogelijk het gevolg zijn van een poging tot fraude.';
+$_lang['quip.err_not_logged_in'] = 'U moet ingelogd zijn om een reactie te kunnen plaatsen.';
+$_lang['quip.hide_deleted'] = 'Verberg verwijdere reacties';
+$_lang['quip.intro_msg'] = 'Discussies en reacties beheren.';
+$_lang['quip.ip'] = 'IP-adres';
+$_lang['quip.latest_comments'] = 'Laatste reacties';
+$_lang['quip.latest_comments_for'] = 'Laatste reacties op';
+$_lang['quip.latest_comments_msg'] = 'Beheer hier de laatste reacties.';
+$_lang['quip.login_to_comment'] = 'Log-in om een reactie te plaatsen.';
+$_lang['quip.message_err_ns'] = 'U hebt geen reactie ingevuld.';
+$_lang['quip.moderated'] = 'Gemodereerd';
+$_lang['quip.moderated_desc'] = 'Nieuwe reacties zullen worden gemodereerd.';
 $_lang['quip.moderators'] = 'Moderators';
-$_lang['quip.moderators_desc'] = 'A comma-separated list of usernames who can moderate this thread.';
-$_lang['quip.moderator_group'] = 'Moderator Group';
-$_lang['quip.moderator_group_desc'] = 'A User Group whose members can moderate this thread.';
-$_lang['quip.name'] = 'Name';
-$_lang['quip.name_err_ns'] = 'Please specify a name.';
-$_lang['quip.no_email_to_specified'] = 'Quip cannot send a spam report email because no admin email was specified.';
-$_lang['quip.notify_me'] = 'Notify of New Replies';
-$_lang['quip.notification'] = 'Notification';
-$_lang['quip.notification_create'] = 'Add Notification';
-$_lang['quip.notification_err_nf'] = 'Notification not found!';
-$_lang['quip.notification_err_ns'] = 'Notification not specified!';
-$_lang['quip.notification_remove'] = 'Remove Notification';
-$_lang['quip.notification_remove_confirm'] = 'Are you sure you want to unsubscribe this user from this Thread?';
-$_lang['quip.notifications'] = 'Notifications';
-$_lang['quip.notifications.intro_msg'] = 'Here is a list of all the subscribed users to this Thread.';
+$_lang['quip.moderators_desc'] = 'Een kommagescheiden lijst van gebruikersnamen die deze disucssie kunnen modereren.';
+$_lang['quip.moderator_group'] = 'Moderatorgroep';
+$_lang['quip.moderator_group_desc'] = 'De gebruikersgroep die deze discussie mogen modereren.';
+$_lang['quip.name'] = 'Naam';
+$_lang['quip.name_err_ns'] = 'Geef een naam op.';
+$_lang['quip.no_email_to_specified'] = 'Quip kan geen spamrapport verzenden omdat er geen e-mailadres voor de beheerder is ingesteld.';
+$_lang['quip.notify_me'] = 'Houd mij op de hoogte van nieuwe reacties';
+$_lang['quip.notification'] = 'Notificatie';
+$_lang['quip.notification_create'] = 'Notificatie toevoegen';
+$_lang['quip.notification_err_nf'] = 'Notificatie niet gevonden';
+$_lang['quip.notification_err_ns'] = 'Notificatie niet gespecificeerd!';
+$_lang['quip.notification_remove'] = 'Notificatie verwijderen';
+$_lang['quip.notification_remove_confirm'] = 'Weet u zeker dat u deze gebruiker wilt uitschrijven voor deze discussie?';
+$_lang['quip.notifications'] = 'Notificaties';
+$_lang['quip.notifications.intro_msg'] = 'Lijst met alle gebruikers die zich hebben ingeschreven voor deze discussie.';
 $_lang['quip.post'] = 'Reageer';
-$_lang['quip.posted'] = 'Geplaatst';
-$_lang['quip.postedon'] = 'Geplaats op:';
-$_lang['quip.preview'] = 'Preview';
-$_lang['quip.recaptcha_err_load'] = 'Could not load reCaptcha service class.';
+$_lang['quip.posted'] = 'Geplaatst op';
+$_lang['quip.postedon'] = 'Geplaats op';
+$_lang['quip.preview'] = 'Voorbeeld';
+$_lang['quip.recaptcha_err_load'] = 'Kan de reCaptcha-serviceclass niet laden.';
 $_lang['quip.reply'] = 'Reageer';
 $_lang['quip.remove'] = 'Verwijder';
-$_lang['quip.remove_selected'] = 'Remove Selected';
-$_lang['quip.report_as_spam'] = 'Report as Spam';
-$_lang['quip.reported_as_spam'] = 'Reported as Spam';
-$_lang['quip.sfs_err_load'] = 'Could not load StopForumSpam class.';
-$_lang['quip.show_deleted'] = 'Show Deleted';
-$_lang['quip.spam_blocked'] = 'Your submission was blocked by a spam filter: [[+fields]]';
-$_lang['quip.spam_email'] = '<p>Hello,</p>
+$_lang['quip.remove_selected'] = 'Selectie verwijderen';
+$_lang['quip.report_as_spam'] = 'Spam rapporteren';
+$_lang['quip.reported_as_spam'] = 'Als spam gerapporteerd';
+$_lang['quip.sfs_err_load'] = 'Kan de StopForumSpam-class niet laden.';
+$_lang['quip.show_deleted'] = 'Toon verwijdere reacties';
+$_lang['quip.spam_blocked'] = 'Uw reactie is geblokkeerd door het spamfilter door mogelijke ongewenste teksten in het volgende veld: [[+fields]]';
+$_lang['quip.spam_email'] = '<p>Hallo,</p>
 
-<p>A comment by [[+username]] has been reported as spam at:</p>
+<p>Een reactie van [[+username]] is als spam gerapporteerd.</p>
 
 <p><a href="[[+url]]">[[+url]]</a></p>
 
-<p>This is an automated email. Please do not reply directly. The
-comment\'s ID is: <strong>[[+id]]</strong></p>
-
-<p>
-Thanks,<br />
-<em>Quip</em>
-</p>';
-$_lang['quip.spam_email_subject'] = '[Quip] Comment Spam Reported';
-$_lang['quip.spam_marked'] = ' - marked as spam.';
-$_lang['quip.subscribed_on'] = 'Subscribed On';
-$_lang['quip.thread'] = 'Reactie-lijst';
-$_lang['quip.thread_autoclosed'] = 'This thread has been closed from taking new comments.';
-$_lang['quip.thread_err_ns'] = 'No thread specified.';
-$_lang['quip.thread_err_remove'] = 'An error occurred while trying to remove the thread.';
-$_lang['quip.thread_err_save'] = 'An error occurred while trying to save the thread.';
-$_lang['quip.thread_manage'] = 'Manage Thread';
-$_lang['quip.thread_msg'] = 'Here you can manage all the comments for this particular thread.';
-$_lang['quip.thread_remove'] = 'Remove Thread';
-$_lang['quip.thread_remove_confirm'] = 'Are you sure you want to completely remove this thread, and all of its comments? This is irreversible.';
-$_lang['quip.thread_remove_selected'] = 'Remove Selected Threads';
-$_lang['quip.thread_remove_selected_confirm'] = 'Are you sure you want to remove these threads entirely? This is irreversible.';
-$_lang['quip.thread_truncate'] = 'Truncate Thread';
-$_lang['quip.thread_truncate_confirm'] = 'Are you sure you want to remove all comments in this thread?';
-$_lang['quip.thread_truncate_selected'] = 'Truncate Selected Threads';
-$_lang['quip.thread_truncate_selected_confirm'] = 'Are you sure you want to remove all comments in these threads?';
-$_lang['quip.threads'] = 'Threads';
-$_lang['quip.unapproved'] = 'Unapproved';
-$_lang['quip.unapproved_comments'] = 'Unapproved Comments';
-$_lang['quip.unapproved_comments_msg'] = 'Moderate any unapproved comments here.';
-$_lang['quip.unsubscribe_me'] = 'Unsubscribe Me from this Thread';
-$_lang['quip.unsubscribed'] = 'You have been unsubscribed from this Thread. Thanks!';
-$_lang['quip.username_said'] = '<strong>[[+username]]</strong> said:';
-$_lang['quip.view'] = 'Bekijk';
+<p>Dit is een geautomatiseerd bericht. U kunt niet op dit bericht reageren. Het ID van de van de reageerder is <strong>[[+id]]</strong></p>';
+$_lang['quip.spam_email_subject'] = 'Reactie als spam gerapporteerd';
+$_lang['quip.spam_marked'] = ' - als spam gerapporteerd.';
+$_lang['quip.subscribed_on'] = 'Ingeschreven op';
+$_lang['quip.thread'] = 'Discussies';
+$_lang['quip.thread_autoclosed'] = 'U kunt niet meer reageren op deze discussie.';
+$_lang['quip.thread_err_ns'] = 'Er is geen discussie gespecificeerd.';
+$_lang['quip.thread_err_remove'] = 'Er is een fout opgetreden bij het verwijderen van de discussie.';
+$_lang['quip.thread_err_save'] = 'Er is een fout opgetreden bij het opslaan van de discussie.';
+$_lang['quip.thread_manage'] = 'Beheer discussie';
+$_lang['quip.thread_msg'] = 'Beheer hier de reacties behorende bij deze discussie.';
+$_lang['quip.thread_remove'] = 'Verwijder discussie';
+$_lang['quip.thread_remove_confirm'] = 'Weet u zeker dat u deze discussie en bijbehorende reacties wilt verwijderen? Dit kan niet meer ongedaan gemaakt worden.';
+$_lang['quip.thread_remove_selected'] = 'Verwijder geselecteerde discussies';
+$_lang['quip.thread_remove_selected_confirm'] = 'Weet u zeker dat u deze discussies wilt verwijderen? Dit kan niet meer ongedaan gemaakt worden.';
+$_lang['quip.thread_truncate'] = 'Verwijder alle reacties uit deze discussie';
+$_lang['quip.thread_truncate_confirm'] = 'Weet u zeker dat u alle reacties uit deze discussie wilt verwijderen?';
+$_lang['quip.thread_truncate_selected'] = 'Verwijder alle reacties uit geselecteerde discussies';
+$_lang['quip.thread_truncate_selected_confirm'] = 'Weet u zeker dat u alle reacties wilt verwijderen uit de geselecteerde discussies?';
+$_lang['quip.threads'] = 'Discussies';
+$_lang['quip.unapproved'] = 'Nog niet gekeurd';
+$_lang['quip.unapproved_comments'] = 'Reacties in afwachting van goedkeuring';
+$_lang['quip.unapproved_comments_msg'] = 'Beheer hier de reacties die in afwachting zijn van goedkeuring.';
+$_lang['quip.unsubscribe_me'] = 'Mij uitschrijven voor deze discussie';
+$_lang['quip.unsubscribed'] = 'U bent nu uitgeschreven voor deze discussie.';
+$_lang['quip.username_said'] = '<strong>[[+username]]</strong> zei:';
+$_lang['quip.view'] = 'Bekijken';
 $_lang['quip.website'] = 'Website';
 
-$_lang['area_tags'] = 'Tags';
-$_lang['setting_quip.allowed_tags'] = 'Toegestaande Tags';
-$_lang['setting_quip.allowed_tags_desc'] = 'The tags allowed for users in comment posting. See <a target="_blank" href="http://php.net/strip_tags">php.net/strip_tags</a> for a list of acceptable values.';
-$_lang['setting_quip.emailsFrom'] = 'From Email';
-$_lang['setting_quip.emailsFrom_desc'] = 'The email address to send system emails from.';
-$_lang['setting_quip.emailsTo'] = 'To Email';
-$_lang['setting_quip.emailsTo_desc'] = 'The email address to send system emails to.';
-$_lang['setting_quip.emailsReplyTo'] = 'Reply-To Email';
-$_lang['setting_quip.emailsReplyTo_desc'] = 'The email address to set the reply-to to. Defaults to emailFrom.';
-$_lang['setting_quip.emails_from_name'] = 'From Name for Emails';
-$_lang['setting_quip.emails_from_name_desc'] = 'The name that will be used for the From: address in emails.';
+$_lang['area_tags'] = 'Labels';
+$_lang['setting_quip.allowed_tags'] = 'Toegestaande labels';
+$_lang['setting_quip.allowed_tags_desc'] = 'Labels die gebruikers mogen gebruiken in een reactie. Zie <a target="_blank" href="http://php.net/strip_tags">php.net/strip_tags</a> voor een lijst van mogelijke waardes.';
+$_lang['setting_quip.emailsFrom'] = 'E-mailadres afzender ("From")';
+$_lang['setting_quip.emailsFrom_desc'] = 'Het e-mailadres dat gebruikt worden als afzender';
+$_lang['setting_quip.emailsTo'] = 'E-mailadres waar berichten naar verzonden worden';
+$_lang['setting_quip.emailsTo_desc'] = 'E-mailadres waar berichten naar verzonden worden';
+$_lang['setting_quip.emailsReplyTo'] = 'Antwoord-aan e-mailadres ("Reply-to")';
+$_lang['setting_quip.emailsReplyTo_desc'] = 'Het e-mailadres wat gebruikt wordt als reply-to. Laat leeg om "emailFrom" te gebruiken.';
+$_lang['setting_quip.emails_from_name'] = 'Naam afzender e-mails';
+$_lang['setting_quip.emails_from_name_desc'] = 'De naam die te ziem is als afzender, samen met het e-mailadres van de afzender.';
